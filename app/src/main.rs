d340b0720fa2bd50d71e4cf4943b676fd4543fa4
christian lansford <christian.lansford@dexcom.com>
2025-06-06 22:38:39 -0400

    add data links to readme

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,6 +3,7 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use hal::gpio::PinState;
 use hal::{pac, prelude::*};
 use panic_halt as _;
 use stm32f4xx_hal as hal;
@@ -13,16 +14,17 @@ fn main() -> ! {
     (pac::Peripherals::take(), cortex_m::peripheral::Peripherals::take())
   {
     let gpioa = dp.GPIOA.split();
-    let mut led = gpioa.pa5.into_push_pull_output();
+    let mut led = gpioa.pa5.into_push_pull_output_in_state(PinState::Low);
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
     let mut delay = cp.SYST.delay(&clocks);
 
     loop {
       led.toggle();
-      delay.delay_ms(1000);
+      delay.delay_ms(5000);
     }
   }
 
+  #[allow(clippy::empty_loop)]
   loop {}
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Rust embedded dev scratchpad
+
+## Links
+
+[datasheet](https://www.st.com/resource/en/datasheet/stm32f446re.pdf)
+[data brief](https://www.st.com/resource/en/data_brief/nucleo-f446re.pdf)
